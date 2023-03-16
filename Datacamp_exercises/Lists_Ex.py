9b1f76943662f07a1768fba99880651ed78ab094
Clement A. Quaye <clemaddo@gmail.com>
2023-03-16 21:41:50 +0000

    Exercises on Lists

---
+++ Datacamp_exercises/Lists_Ex.py
@@ -0,0 +1,38 @@
+"""
+Question 1.
+Create a list, areas, that contains the area of the hallway (hall), kitchen (kit), living room (liv), bedroom (bed) and bathroom (bath), in this order. 
+Use the predefined variables.
+Print areas with the print() function
+"""
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Create list areas
+areas = [hall, kit, liv, bed, bath]
+
+"""
+Finish the code that creates the areas list. Build the list so that the list first contains the name of each room as a string and then its area. In other words, add the strings "hallway", "kitchen" and "bedroom" at the appropriate locations.
+
+# Print areas
+print(areas)
+"""
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Adapt list areas
+areas = ["hallway",hall,"kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath]
+
+# Print areas
+print(areas)
+
+
+#List of Lists
